b336aa3465021d6e8881ff7f0bab506de7c4be99
Abhinav Gupta <mail@abhinavg.net>
2023-04-07 13:37:18 -0700

    test: Automatic suite, add more tests (#8)
    In q_test, automatically pick up test methods
    instead of manually specifying them with t.Run.
    This is similar to the way testify/suite does it,
    but passing testing.T as an argument explicitly.
    
    Add a couple more tests.

--- q_test.go
+++ q_test.go
@@ -2,7 +2,9 @@ package ring_test
 
 import (
 	"fmt"
+	"reflect"
 	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -47,12 +49,21 @@ func testQueueSuite(t *testing.T, newWithCap func(capacity int) *ring.Q[int]) {
 					NumItems: size,
 				}
 
-				t.Run("Empty", suite.TestEmpty)
-				t.Run("PushPop", suite.TestPushPop)
-				t.Run("PushPopInterleaved", suite.TestPushPopInterleaved)
-				t.Run("PushPopWraparound", suite.TestPushPopWraparound)
-				t.Run("Snapshot", suite.TestSnapshot)
-				t.Run("SnapshotReuse", suite.TestSnapshotReuse)
+				suitev := reflect.ValueOf(suite)
+				suitet := suitev.Type()
+				for i := 0; i < suitet.NumMethod(); i++ {
+					name, ok := cutPrefix(suitet.Method(i).Name, "Test")
+					if !ok {
+						continue
+					}
+
+					testfn, ok := suitev.Method(i).Interface().(func(*testing.T))
+					if !ok {
+						continue
+					}
+
+					t.Run(name, testfn)
+				}
 			})
 		}
 	}
@@ -76,6 +87,8 @@ func (s *queueSuite) TestEmpty(t *testing.T) {
 	assert.Panics(t, func() {
 		q.Peek()
 	}, "pop")
+
+	assert.Empty(t, q.Snapshot(nil), "snapshot")
 
 	q.Do(func(item int) bool {
 		t.Errorf("unexpected item: %v", item)
@@ -152,6 +165,47 @@ func (s *queueSuite) TestPushPopWraparound(t *testing.T) {
 	assert.Equal(t, want, got, "items")
 }
 
+func (s *queueSuite) TestDo(t *testing.T) {
+	q := s.NewEmpty()
+	want := make([]int, 0, s.NumItems)
+	for i := 0; i < s.NumItems; i++ {
+		q.Push(i)
+		want = append(want, i)
+	}
+
+	got := make([]int, 0, s.NumItems)
+	q.Do(func(i int) bool {
+		got = append(got, i)
+		return true
+	})
+	assert.Equal(t, want, got, "do did not iterate fully")
+}
+
+func (s *queueSuite) TestDoReturnEarly(t *testing.T) {
+	stopAt := s.NumItems / 2
+
+	q := s.NewEmpty()
+	want := make([]int, 0, s.NumItems)
+	for i := 0; i < s.NumItems; i++ {
+		q.Push(i)
+		if i < stopAt {
+			want = append(want, i)
+		}
+	}
+
+	got := make([]int, 0, stopAt)
+	q.Do(func(i int) bool {
+		if i >= stopAt {
+			return false
+		}
+
+		got = append(got, i)
+		return true
+	})
+
+	assert.Equal(t, want, got, "iterated over unexpected items")
+}
+
 func (s *queueSuite) TestSnapshot(t *testing.T) {
 	t.Parallel()
 
@@ -183,4 +237,13 @@ func (s *queueSuite) TestSnapshotReuse(t *testing.T) {
 	for _, item := range snap[1:] {
 		assert.Equal(t, item, q.Pop(), "item")
 	}
+}
+
+// Copy of strings.CutPrefix for Go 1.19.
+// Delete once Go 1.20 is minimum supported version.
+func cutPrefix(s, prefix string) (after string, found bool) {
+	if !strings.HasPrefix(s, prefix) {
+		return s, false
+	}
+	return s[len(prefix):], true
 }
